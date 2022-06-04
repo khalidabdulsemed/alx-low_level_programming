c505709e98c86db77887a6fb72cddb24dfe5f1a2
Khalid Abdulsemed <khalidabdulsemed79@gmail.com>
2022-06-04 10:03:02 -0700

    My first C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  *  * main - Entry point
  *   *
- *    * Description: prints "Programming is like building a multilingual puzzle"
+ *    * Description: prints "Programming is like building a multilingual"
  *     *
  *      * Return: Always 0 (Success)
  *       */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+		return (0);
 }
