48085bdc6ea9ebff4d1bfc554fa6d3d8fb01dfac
Khalid Abdulsemed <khalidabdulsemed79@gmail.com>
2022-06-04 10:28:04 -0700

    I am printing something

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,13 @@
 /**
  *  * main - Entry point
  *   *
- *    * Description: prints the below program
- *     *
- *      * Return 0 if the program works fine
- *       */
-
+ *    * Description: prints "with proper grammar, but the outcome is a piece of
+ *     * art,"
+ *      *
+ *       * Return: Always 0 (Success)
+ *        */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
