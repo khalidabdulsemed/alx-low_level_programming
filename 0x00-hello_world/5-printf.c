14f13c3a7f5428fd0d89625e8b624534161906b5
Khalid Abdulsemed <khalidabdulsemed79@gmail.com>
2022-06-04 10:18:35 -0700

    I am printing something

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: prints the below program
+ *     *
+ *      * Return 0 if the program works fine
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
