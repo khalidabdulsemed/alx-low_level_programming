c9c4a352af068912f5a31d7e8e83955ef336fef7
Khalid Abdulsemed <khalidabdulsemed79@gmail.com>
2022-06-05 12:49:02 -0700

    Last digit determiner

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,7 +7,7 @@
  *    *    * Description: determines the last digit of a number
  *     *     *
  *      *      * Return: Always 0 (Success)
- *        */
+ */
 
 int main(void)
 {
@@ -17,15 +17,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	x = n % 10;
-	if (n > 5)
+	if (x > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, x);
 	} else
-	if (n == 0)
+	if (x == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, x);
 	} else
-	if (n > 6 && n != 0)
+	if (x > 6 && n != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 	}
