10b3532517eb9207d303c28d8c48875f0b138f3a
Khalid Abdulsemed <khalidabdulsemed79@gmail.com>
2022-06-07 11:14:36 -0700

    I am printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  *  *  * main - Entry point
+ *   *   *   *
+ *    *    *    * Description: determines the last digit of a number
+ *     *     *     *
+ *      *      *      * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar");
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+I am learning C. I hope it will be easy. and I havent faced that much challenges in C.
