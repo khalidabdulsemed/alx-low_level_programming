e070c3a030832d81f1f72f98695d31ddc92d08c6
Khalid Abdulsemed <khalidabdulsemed79@gmail.com>
2022-06-07 11:25:34 -0700

    I am printing _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
  *  *  *  * main - Entry point
  *   *   *   *
@@ -9,5 +10,6 @@
 
 int main(void)
 {
-	printf("_putchar");
+	_putchar("_putchar");
+	return (0);
 }
