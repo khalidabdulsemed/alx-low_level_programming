448ce212a1c5669e0e08fd9138115d58392262c0
Khalid Abdulsemed <khalidabdulsemed79@gmail.com>
2022-06-09 09:01:41 -0700

    Numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *  *  *  * main - Entry point
+ *   *   *   *
+ *    *    *    * Description: a simple if and else statement
+ *     *     *     *
+ *      *      *      * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 48;
+
+	while (x < 58)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}	
