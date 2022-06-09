e46dad76a61c2da82c4d36ed4b807ab3e72807f9
Khalid Abdulsemed <khalidabdulsemed79@gmail.com>
2022-06-09 09:47:18 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *  *  *  * main - Entry point
+ *   *   *   *
+ *    *    *    * Description: a simple if and else statement
+ *     *     *     *
+ *      *      *      * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+
+	while (i <= 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	int a = 'a';
+
+	while (a <= 'f')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n')
+
+
+	return (0);
