af552ff624d92742cade76c0cf9897f283860007
Khalid Abdulsemed <khalidabdulsemed79@gmail.com>
2022-06-09 06:38:55 -0700

    alphabets except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *  *  *  *  * main - Entry point
+ *   *   *   *   *
+ *    *    *    *    * Description: determines the last digit of a number
+ *     *     *     *     *
+ *      *      *      *      * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= z)
+	{
+		if (a != q && a != e)
+		{
+			putchar(a);
+		}
+	a++;
+}
