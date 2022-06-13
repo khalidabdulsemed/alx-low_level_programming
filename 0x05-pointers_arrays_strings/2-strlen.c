d81c4bd79dceb289221d3562076c32635be0d81d
Khalid Abdulsemed <khalidabdulsemed79@gmail.com>
2022-06-13 09:05:22 -0700

    pointers, arrahs and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,19 @@
 #include "main.h"
 
+/**
+ *  * _strlen - length of a string
+ *   * @s: input char
+ *    * Return: length of a string
+ */
+
 int _strlen(char *s)
 {
-	int len;
-	len = strlen(*s);
+	int len = 0;
+	
+	while (len != \0)
+	{
+		s++;
+		l++;
+	}
+	return (len);
 }
