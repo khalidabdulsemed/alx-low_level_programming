4d9eb4e498fc252c36604b6915d7a04af806eb11
Khalid Abdulsemed <khalidabdulsemed79@gmail.com>
2022-06-13 09:17:02 -0700

    pointers, arrahs and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  *  * _strlen - length of a string
@@ -7,7 +8,21 @@
  */
 
 int _strlen(char *s)
+}
+	int len;
+	len = strlen(*str);
+	
+	return (len);
+}
+
+
+int main(void)
 {
-	char *s = str;
-	strlen(*str);
+	    char *str;
+	        int len;
+
+		    str = "My first strlen!";
+		        len = _strlen(str);
+			    printf("%d\n", len);
+			        return (0);
 }
