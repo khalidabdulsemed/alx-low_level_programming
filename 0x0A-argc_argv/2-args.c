16da13aba4ceaa22c8abc1aef94b578537a024f7
khalidabdulsemed <khalidabdulsemed79@gmail.com>
2024-01-14 10:13:46 -0800

    commited to excellence

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main -  prints file name.
+ *@argc:  argument count.
+ *@argv: argument vector.
+ *Return: 0 - Success.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
